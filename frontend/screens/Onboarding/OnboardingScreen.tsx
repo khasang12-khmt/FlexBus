0460a31baf44ccd760139b309f06081b04f0cd52
= <=>
2023-05-14 00:50:49 +0700

    style: add style for onboarding

--- frontend/screens/Onboarding/OnboardingScreen.tsx
+++ frontend/screens/Onboarding/OnboardingScreen.tsx
@@ -1,10 +1,18 @@
-import { View, Text, Image, Button } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  useWindowDimensions,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Onboarding from "react-native-onboarding-swiper";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { IconButton, Button } from "react-native-paper";
 
 const OnboardingScreen = () => {
   const navigation = useNavigation<any>();
+  const windowWidth = 0.85 * useWindowDimensions().width;
 
   const DotComponent = ({ selected }: { selected: boolean }) => {
     return (
@@ -17,35 +25,72 @@ const OnboardingScreen = () => {
   };
 
   const Next = ({ ...props }) => (
-    <View>
-      <MaterialCommunityIcons
-        name="chevron-right"
+    <View className="bg-[#001356] rounded-lg h-12 w-12 justify-center items-center mr-5">
+      <IconButton
+        icon="chevron-right"
+        iconColor="#F7FAF9"
         size={40}
-        color={"#001356"}
         {...props}
-      />
+      ></IconButton>
     </View>
   );
 
-  // const Done = ({ ...props }: { allowFontScaling: boolean }) => (
-  //   <Button title="Get Started" {...props} />
-  // );
+  const Skip = ({ ...props }) => (
+    <View className="ml-2 rounded-lg">
+      <Button textColor="#001356" labelStyle={{ fontSize: 17 }} {...props}>
+        Skip
+      </Button>
+    </View>
+  );
+
+  const Done = ({ ...props }) => (
+    <View className="bg-[#001356] rounded-lg h-12 w-12 justify-center items-center mr-5">
+      <IconButton
+        icon="check-bold"
+        iconColor="#F7FAF9"
+        size={25}
+        {...props}
+      ></IconButton>
+    </View>
+  );
 
   return (
     <Onboarding
       onSkip={() => navigation.replace("AppStack")}
       onDone={() => navigation.replace("AppStack")}
-      bottomBarColor="#FFFFFF"
+      bottomBarColor="#F7FAF9"
+      bottomBarHeight={80}
       NextButtonComponent={Next}
+      SkipButtonComponent={Skip}
       DotComponent={DotComponent}
-      // DoneButtonComponent={Done}
+      DoneButtonComponent={Done}
+      containerStyles={{
+        backgroundColor: "#F7FAF9",
+        // justifyContent: "flex-start",
+      }}
+      imageContainerStyles={{
+        height: 300,
+        // backgroundColor: "#000000"
+      }}
+      titleStyles={{
+        fontFamily: "RobotoBold",
+        color: "#001356",
+      }}
+      subTitleStyles={{
+        fontFamily: "RobotoRegular",
+        color: "#001B3F",
+        maxWidth: "70%",
+      }}
       pages={[
         {
           backgroundColor: "#FFFFFF",
           image: (
             <Image
               source={require("../../assets/Onboarding1.png")}
-              className="object-none"
+              className="w-10/12"
+              style={{
+                resizeMode: "contain",
+              }}
             />
           ),
           title: "Open FlexBus",
@@ -57,7 +102,10 @@ const OnboardingScreen = () => {
           image: (
             <Image
               source={require("../../assets/Onboarding2.png")}
-              className="object-none"
+              className="w-10/12"
+              style={{
+                resizeMode: "contain",
+              }}
             />
           ),
           title: "Wait at closest",
@@ -69,7 +117,10 @@ const OnboardingScreen = () => {
           image: (
             <Image
               source={require("../../assets/Onboarding3.png")}
-              className="object-none"
+              className="w-10/12"
+              style={{
+                resizeMode: "contain",
+              }}
             />
           ),
           title: "Take right bus",
